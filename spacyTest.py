742cdf49ac91731d194eb63d887443d9984da964
RileyHal <halbertriley@gmail.com>
2022-10-06 17:53:25 -0400

    Lemmatization and Stop Word Removal

--- spacyTest.py
+++ spacyTest.py
@@ -2,27 +2,38 @@ import spacy
 import DocumentReader
 from spacy import displacy
 
+nlp = spacy.load("en_core_web_sm")
+stopWords = nlp.Defaults.stop_words
 
-nlp = spacy.load("en_core_web_sm");
+#DocText = DocumentReader.Read('ExampleData/USCODE-2020-title1.pdf')
+#DocText = DocumentReader.Read('ExampleData/story.txt')
+DocText = DocumentReader.Read('ExampleData/example.txt')
 
-#DocumentText = PDFReader.Read('ExampleData/USCODE-2020-title1.pdf')
-DocText = DocumentReader.Read('ExampleData/story.txt')
+
 
 doc = nlp(DocText)
+#sentences = list(doc.sents)
 
-# for token in doc:
-#     print(token)
+print("Starting Text\nToken Count: " + str(len(doc)) + "\n\t" + DocText + "\n")
 
-# for sent in doc.sents:
-#     print(sent)
+noStopWords = ''
+for token in doc:
+    if token.is_stop == False:
+        noStopWords += token.text + ' '
+doc = nlp(noStopWords)
+print("Text with no stop words\nToken Count: " + str(len(doc)) + "\n\t" + noStopWords + "\n")
 
-sentences = list(doc.sents)
+noStopWordsAndLemmatization = ''
+for token in doc:
+    noStopWordsAndLemmatization += token.lemma_ + ' '
+doc = nlp(noStopWordsAndLemmatization)
+print("Text with no stop words + lemmatization\nToken Count: " + str(len(doc)) + "\n\t" + noStopWordsAndLemmatization + "\n")
 
-for sentence in sentences:
-    for token in sentence:
-        print(token.text + ": " + token.ent_type_, token.pos_, token.dep_)
-
-    print("\n")
+# for sentence in sentences:
+#     for token in sentence:
+#         print(token.text + ": " + token.ent_type_, token.pos_, token.dep_)
+#     print("\n")
 
 print("http://localhost:5000")
-displacy.serve(sentences[0], style="dep")
+#Display styles ['ent', 'dep', 'span']
+displacy.serve(doc, style='dep')
