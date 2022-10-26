80fc8749a2410f15492e3a0b9de3feea6e3df7fa
RileyHal <halbertriley@gmail.com>
2022-10-26 10:13:29 -0400

    Added CoReferences

--- README.md
+++ README.md
@@ -5,6 +5,10 @@ File ReSort is an application that uses
 ## Installation
 
 Download [python](https://www.python.org/downloads/) and the package manager [pip](https://pip.pypa.io/en/stable/). Before running the application you must follow the spaCy install directions from [spaCy.io](https://spacy.io/usage)
+python -m pip install coreferee
+python -m coreferee install en
+python -m spacy download en_core_web_lg
+python -m spacy download en_core_web_trf
 
 ## Contributing
 Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
--- spacyTest.py
+++ spacyTest.py
@@ -1,39 +1,51 @@
 import spacy
+import coreferee
 import DocumentReader
 from spacy import displacy
 
-nlp = spacy.load("en_core_web_sm")
-stopWords = nlp.Defaults.stop_words
+nlp = spacy.load('en_core_web_sm')
+nlp.add_pipe('coreferee')
+# stopWords = nlp.Defaults.stop_words
 
 #DocText = DocumentReader.Read('ExampleData/USCODE-2020-title1.pdf')
-#DocText = DocumentReader.Read('ExampleData/story.txt')
-DocText = DocumentReader.Read('ExampleData/example.txt')
-
-
+DocText = DocumentReader.Read('ExampleData/story.txt')
+#DocText = DocumentReader.Read('ExampleData/example.txt')
+#DocText = DocumentReader.Read('ExampleData/test.txt')
 
 doc = nlp(DocText)
-#sentences = list(doc.sents)
+doc._.coref_chains.print()
+print(doc._.coref_chains.resolve(doc[27]))
+
+for chain in doc._.coref_chains:
+    print(chain)
+    #print(doc._.coref_chains.resolve(chain))
+
+sentences = list(doc.sents)
 
 print("Starting Text\nToken Count: " + str(len(doc)) + "\n\t" + DocText + "\n")
 
-noStopWords = ''
-for token in doc:
-    if token.is_stop == False:
-        noStopWords += token.text + ' '
-doc = nlp(noStopWords)
-print("Text with no stop words\nToken Count: " + str(len(doc)) + "\n\t" + noStopWords + "\n")
-
-noStopWordsAndLemmatization = ''
-for token in doc:
-    noStopWordsAndLemmatization += token.lemma_ + ' '
-doc = nlp(noStopWordsAndLemmatization)
-print("Text with no stop words + lemmatization\nToken Count: " + str(len(doc)) + "\n\t" + noStopWordsAndLemmatization + "\n")
+# noStopWords = ''
+# for token in doc:
+#     if token.is_stop == False and token.text not in ['"', ',', '.', '!', '?']:
+#         noStopWords += ' ' + token.text
+#     elif token.is_stop == False:
+#         noStopWords += token.text
+# doc = nlp(noStopWords)
+# print("Text with no stop words\nToken Count: " + str(len(doc)) + "\n\t" + noStopWords + "\n")
+#
+# noStopWordsAndLemmatization = ''
+# for token in doc:
+#     noStopWordsAndLemmatization += token.lemma_ + ' '
+# doc = nlp(noStopWordsAndLemmatization)
+# print("Text with no stop words + lemmatization\nToken Count: " + str(len(doc)) + "\n\t" + noStopWordsAndLemmatization + "\n")
 
 # for sentence in sentences:
 #     for token in sentence:
 #         print(token.text + ": " + token.ent_type_, token.pos_, token.dep_)
 #     print("\n")
 
-print("http://localhost:5000")
 #Display styles ['ent', 'dep', 'span']
-displacy.serve(doc, style='dep')
+p = displacy.render(doc, style='ent')
+displacy.serve(doc, style='ent')
+print(p)
+
