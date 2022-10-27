01e2421ee5d9d0391a2dafe5ede75b322e313ab0
RileyHal <halbertriley@gmail.com>
2022-10-27 19:17:16 -0400

    more cleanup

--- spacyTest.py
+++ spacyTest.py
@@ -13,6 +13,30 @@ DocText = DocumentReader.Read('ExampleData/example.txt')
 #DocText = DocumentReader.Read('ExampleData/test.txt')
 
 doc = nlp(DocText)
+doc._.coref_chains.print()
+
+#[parentDocIndex: [childDocIndex,childDocIndex,...]]
+chainDict = {}
+
+for chain in doc._.coref_chains:
+    mentionIndexList = []
+    for mention in chain:
+        mentionIndexList.append(mention.token_indexes[0])
+    chainDict[chain.index] = mentionIndexList
+
+#[childDocIndex: parentDocIndex]
+mentionDict = {}
+
+for key in chainDict.keys():
+    parentIndex = chainDict[key][0]
+    for x in range(1, len(chainDict[key])):
+        mentionDict[chainDict[key][x]] = parentIndex
+
+del chainDict
+
+#mention dict is in format of [childDocIndex: parentDocIndex]. This will help to reduce duplicate nodes in our graph
+#when we actually start creating nodes.
+print(mentionDict)
 
 #Rule based matching, Matcher and DependencyMatcher
 # matcher = DependencyMatcher(nlp.vocab)
@@ -28,29 +52,8 @@ doc = nlp(DocText)
 #
 # print("Matches:", [doc[start:end].text for match_id, start, end in matches])
 
-doc._.coref_chains.print()
-
-chainDict = {}
-
-for chain in doc._.coref_chains:
-    mentionIndexList = []
-    for mention in chain:
-        mentionIndexList.append(mention.token_indexes[0])
-    chainDict[chain.index] = mentionIndexList
-
-mentionDict = {}
-
-for key in chainDict.keys():
-    parentIndex = chainDict[key][0]
-    for x in range(1, len(chainDict[key])):
-        mentionDict[chainDict[key][x]] = parentIndex
-
-del chainDict
-
-print(mentionDict)
-
 #Display styles ['ent', 'dep', 'span']
 #p = displacy.render(doc, style='ent')
-displacy.serve(doc, style='dep')
+displacy.serve(doc, style='ent')
 
 
