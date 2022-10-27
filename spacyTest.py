2e9d83bd15445ca18023933adb3247b80ebf86c7
RileyHal <halbertriley@gmail.com>
2022-10-27 18:00:56 -0400

    CoReference support and cleanup

--- spacyTest.py
+++ spacyTest.py
@@ -2,50 +2,55 @@ import spacy
 import coreferee
 import DocumentReader
 from spacy import displacy
+from spacy.matcher import DependencyMatcher
 
-nlp = spacy.load('en_core_web_sm')
+nlp = spacy.load('en_core_web_trf')
 nlp.add_pipe('coreferee')
-# stopWords = nlp.Defaults.stop_words
 
 #DocText = DocumentReader.Read('ExampleData/USCODE-2020-title1.pdf')
-DocText = DocumentReader.Read('ExampleData/story.txt')
-#DocText = DocumentReader.Read('ExampleData/example.txt')
+#DocText = DocumentReader.Read('ExampleData/story.txt')
+DocText = DocumentReader.Read('ExampleData/example.txt')
 #DocText = DocumentReader.Read('ExampleData/test.txt')
 
 doc = nlp(DocText)
+
+#Rule based matching, Matcher and DependencyMatcher
+# matcher = DependencyMatcher(nlp.vocab)
+#
+# patterns = [
+#    [{"POS": "PROPN"}, {"POS": "VERB"}, {"POS": "PROPN"}],
+#    [{"POS": "NOUN"}, {"POS": "VERB"}, {"POS": "NOUN"}]
+# ]
+#
+# matcher.add("uniqueID", patterns, on_match=newFunc)
+#
+# matches = matcher(doc)
+#
+# print("Matches:", [doc[start:end].text for match_id, start, end in matches])
+
 doc._.coref_chains.print()
-print(doc._.coref_chains.resolve(doc[27]))
+
+chainDict = {}
 
 for chain in doc._.coref_chains:
-    print(chain)
-    #print(doc._.coref_chains.resolve(chain))
+    mentionIndexList = []
+    for mention in chain:
+        mentionIndexList.append(mention.token_indexes[0])
+    chainDict[chain.index] = mentionIndexList
 
-sentences = list(doc.sents)
+mentionDict = {}
 
-print("Starting Text\nToken Count: " + str(len(doc)) + "\n\t" + DocText + "\n")
+for key in chainDict.keys():
+    parentIndex = chainDict[key][0]
+    for x in range(1, len(chainDict[key])):
+        mentionDict[chainDict[key][x]] = parentIndex
 
-# noStopWords = ''
-# for token in doc:
-#     if token.is_stop == False and token.text not in ['"', ',', '.', '!', '?']:
-#         noStopWords += ' ' + token.text
-#     elif token.is_stop == False:
-#         noStopWords += token.text
-# doc = nlp(noStopWords)
-# print("Text with no stop words\nToken Count: " + str(len(doc)) + "\n\t" + noStopWords + "\n")
-#
-# noStopWordsAndLemmatization = ''
-# for token in doc:
-#     noStopWordsAndLemmatization += token.lemma_ + ' '
-# doc = nlp(noStopWordsAndLemmatization)
-# print("Text with no stop words + lemmatization\nToken Count: " + str(len(doc)) + "\n\t" + noStopWordsAndLemmatization + "\n")
+del chainDict
 
-# for sentence in sentences:
-#     for token in sentence:
-#         print(token.text + ": " + token.ent_type_, token.pos_, token.dep_)
-#     print("\n")
+print(mentionDict)
 
 #Display styles ['ent', 'dep', 'span']
-p = displacy.render(doc, style='ent')
-displacy.serve(doc, style='ent')
-print(p)
+#p = displacy.render(doc, style='ent')
+displacy.serve(doc, style='dep')
+
 
